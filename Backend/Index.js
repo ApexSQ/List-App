18a5c8d2ff1121d522d01bf0aaa740d7ac2335ed
ApexSQ <davies.joshua@icloud.com>
2024-07-09 13:25:04 +1000

    U

--- Backend/Index.js
+++ Backend/Index.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 app.get('/', function (req, res) {
-  res.send('Hello WorldDDDD')
+  res.send('Hello Nodmonkey')
 })
 
 app.listen(3000, function () {
     console.log('listening on port 3000');
 
 })
+
+// setting up middleware
+
+app.use(express.json())
+app.use(express.urlencoded({ extended : true }))
+
+app.use(cors("*")); 
