0f594f2f334aca38e1beafdf393e0195846a01f9
ApexSQ <davies.joshua@icloud.com>
2024-07-09 14:16:10 +1000

    Mongoose

--- Backend/Index.js
+++ Backend/Index.js
@@ -1,19 +1,34 @@
 const express = require('express')
+
+//const cors = require('cors')
+
+const mongoose = require('mongoose');
 const app = express()
-const cors = require('cors')
 
-app.get('/', function (req, res) {
-  res.send('Hello Nodmonkey')
-})
-
-app.listen(3000, function () {
-    console.log('listening on port 3000');
-
-})
 
 // setting up middleware
-
 app.use(express.json())
 app.use(express.urlencoded({ extended : true }))
 
-app.use(cors("*")); 
+
+//app.use(cors("*")); 
+
+
+app.get("/", function (req, res) {
+  res.send('Hello Nodmonkey')
+})
+
+
+
+const ConnectionString = "mongodb+srv://djosh734:jdavies01@cluster0.0qmyd6j.mongodb.net/";
+
+mongoose.connect(ConnectionString, {}).then(() => {
+  console.log("connected to database");
+
+  app.listen(3000, function() {
+    console.log('Server running on port 3000');
+  });
+})
+.catch((error) => {
+  console.error('Error connecting to database:', error);
+});
