3bdf9a33cbc544de62a7162829fc8dcb63f8bb0e
ApexSQ <davies.joshua@icloud.com>
2024-07-15 15:10:57 +1000

    zz

--- Backend/Index.js
+++ Backend/Index.js
@@ -1,99 +1,119 @@
-//importing usign common js
-const express = require("express");
+const express = require('express');
 const cors = require("cors");
-const mongoose = require("mongoose");
-
-//importing using es module
-// import express from "express";
+const mongoose = require('mongoose');
 
 const app = express();
 
-//setting middleware
-app.use(express.json()); //
-app.use(express.urlencoded({ extended: true }));
-
+// setting up middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended : true }));
 app.use(cors("*"));
 
-//Importing model
-const TodoModel = require("./model/Todo");
+// importing model
+const ListAppModel = require("./model/ListApp");
 
-const connectionString = "mongodb+srv://nidup1998:learningtodo@cluster0.ivup0cn.mongodb.net/todoDB"; //we added name of the database at the end of the string
+const ConnectionString = "mongodb+srv://djosh734:jdavies01@cluster0.0qmyd6j.mongodb.net/ListAppDB";
 
-mongoose
-	.connect(connectionString)
-	.then(() => {
-		console.log("Connected to the database");
+mongoose.connect(ConnectionString).then(() => {
+
+		console.log("Connected to the databasez");
 		app.listen(3000, function () {
 			console.log("server running at port 3000");
+
 		});
+    
 	})
 	.catch((err) => console.log(err));
 
-//CRUD Opertaions
+// CRUD Operations
 
-//get method
-//Read method
-app.get("/todos", async (req, res) => {
-	// res.send("Hello Perth!");
-	try {
-		const response = await TodoModel.find({});
 
-		// console.log(response);
 
-		res.json(response);
-	} catch (err) {
-		console.log(err);
-	}
+// Get Method
+app.get("/lists", async (req, res) => {
+  try {
+
+    const response = await ListAppModel.find({});
+
+    console.log(response);
+
+    res.json(response);
+
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Error fetching datazzz', error: error.message });
+  }
 });
 
-//Create method
-app.post("/todos", async (req, res) => {
-	try {
-		console.log(req.body);
+ 
 
-		const todo = req.body;
 
-		//add the new item to the database
-		const newItem = await TodoModel.create(todo);
+// Create Method PostMethod
 
-		// res.send("Your post method is working");
-		res.status(200).send("Successful");
-	} catch (error) {
-		console.log(error);
-		res.status(500).send("Server Error");
-	}
+app.post("/lists", async (req, res) => {
+  try {
+
+    console.log(req.body);
+
+    const list = req.body;
+   
+    const newItem = await ListAppModel.create(list);
+
+    res.status(200).send( "successfull");
+    
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ message: 'Error fetching data', error: error.message });
+  }
+
 });
 
-//Delete method
-app.delete("/todos/:id", async (req, res) => {
-	try {
-		let id = req.params.id;
 
-		console.log(id);
-		const deletedItem = await TodoModel.deleteOne({
-			_id: id,
-		});
+//delete Method
 
-		res.status(200).send("Delete Successful");
-	} catch (error) {
-		console.log(error);
-		res.status(500).send("Server Error");
-	}
+app.delete("/lists/:id", async (req, res) => {
+  try {
+
+    let id = req.params.id;
+    console.log(id);
+
+    const deleteItem = await ListAppModel.deleteOne({
+      _id: id
+    });
+
+    res.status(200).send( " DELETE success")
+
+    
+    
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ message: 'Error fetching data', error: error.message });
+  }
+
 });
 
-app.put("/todos/:id", async (req, res) => {
-	try {
-		const id = req.params.id;
-		console.log(id);
+// put Method
 
-		const { text } = req.body;
 
-		const updateOptions = { text: text };
-		const updateItem = await TodoModel.findByIdAndUpdate(id, updateOptions);
+app.put("/lists/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const { text, status } = req.body;
 
-		res.status(200).send("Updated item");
-	} catch (error) {
-		console.log(error);
-		res.status(500).send("Server Error");
-	}
+    const updatedItem = await ListAppModel.findByIdAndUpdate(
+      id,
+      { $set: { text: text, status: status } },
+      { new: true, runValidators: true }
+    );
+
+    if (!updatedItem) {
+      return res.status(404).json({ message: 'Item not found' });
+    }
+
+    res.status(200).json(updatedItem);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error updating item', error: error.message });
+  }
 });
+
